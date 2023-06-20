b7bc04c36bd8e0a063817b5e44ff73f4f0f2234e
Gabriel Chang <mr.gabriel.chang@gmail.com>
2023-06-20 11:29:30 +0800

    Made main content fill page so footer is at the bottom

--- src/scripts/asideNavHelper.ts
+++ src/scripts/asideNavHelper.ts
@@ -2,27 +2,26 @@ const sections = document.querySelectorAll('section');
 const navList = document.querySelectorAll('.aside-nav-link');
 const navBar = document.querySelector('#nav-bar') as HTMLElement;
 const navBarHeight = navBar.offsetHeight;
-let sectionMap: Map<number, string> = new Map;
-sections.forEach((section) => { 
-    const sectionID = section.getAttribute('id');
-    if (sectionID === null) {
-        throw new Error('Section does not have an id')
+let sectionMap: Map<number, string> = new Map();
+sections.forEach((section) => {
+  const sectionID = section.getAttribute('id');
+  if (sectionID === null) {
+    throw new Error('Section does not have an id');
+  }
+  sectionMap.set(section.offsetTop - navBarHeight - 5, sectionID);
+});
+export const handleScroll = () => {
+  let current = '';
+  sectionMap.forEach((sectionID, sectionTop) => {
+    if (scrollY >= sectionTop) {
+      current = sectionID;
     }
-    sectionMap.set(section.offsetTop - navBarHeight - 5, sectionID);
- })
-export const handleScroll = () => { 
-    let current = '';
-    sectionMap.forEach((sectionID, sectionTop) => { 
-        if (scrollY >= sectionTop ) {
-            current = sectionID;
-        }
-     })
+  });
 
-    for (const li of navList) {
-        li.classList.remove('active');
-        if (li.getAttribute('data-for') === current) {
-            li.classList.add('active');
-        }
+  for (const li of navList) {
+    li.classList.remove('active');
+    if (li.getAttribute('data-for') === current) {
+      li.classList.add('active');
     }
-
- }
+  }
+};
