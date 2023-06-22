11a7c5ef16eacc9a2565699084c29baf85bbeaf1
Gabriel Chang <mr.gabriel.chang@gmail.com>
2023-06-22 17:34:54 +0800

    Animated the hamburger

--- src/scripts/mobileNavHelper.ts
+++ src/scripts/mobileNavHelper.ts
@@ -5,12 +5,10 @@ export function toggleNav() {
     console.log('Nav toggle button or Nav not found');
     return;
   }
-  if (navLinks.classList.contains('visible')) {
-    toggleBtn.innerHTML =
-      '<svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48" class="icon"><path d="M120-240v-60h720v60H120Zm0-210v-60h720v60H120Zm0-210v-60h720v60H120Z"/></svg>';
-  } else {
-    toggleBtn.innerHTML =
-      '<svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48" class="icon"><path d="m249-207-42-42 231-231-231-231 42-42 231 231 231-231 42 42-231 231 231 231-42 42-231-231-231 231Z"/></svg>';
-  }
   navLinks.classList.toggle('visible');
+  if (navLinks.classList.contains('visible')) {
+    toggleBtn.setAttribute('aria-expanded', 'true');
+  } else {
+    toggleBtn.setAttribute('aria-expanded', 'false');
+  }
 }
